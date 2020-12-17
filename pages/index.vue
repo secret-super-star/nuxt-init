55d4fddbe4411e9be288a2d735633db7629ef580
secret-super-star <mjone352@gmail.com>
2020-12-17 21:52:13 +0800

    error page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <h1>Home page</h1>
-    <NuxtLink to="/about">
+    <NuxtLink to="/posts">
       About (internal link that belongs to the Nuxt App)
     </NuxtLink>
     <a href="https://nuxtjs.org">External Link to another page</a>
---
+++ pages/posts.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- Your template -->
+  <div>
+      
+  </div>
+</template>
+<script>
+  export default {
+    layout: 'blog'
+    // page component definitions
+  }
+</script>
